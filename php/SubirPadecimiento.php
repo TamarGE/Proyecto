68ecd5db8c7dd52032d8b1e2c7193e19a97877c0
GUERSON EMILIA <emiliaguerson@gmail.com>
2022-10-19 15:49:34 -0300

    Subir Padecimientos
    Empecé a escribir el código para subir los padecimientos editados y los nuevos. Tengo que probar todo el código relacionado con la base de datos

---
+++ php/SubirPadecimiento.php
@@ -0,0 +1,44 @@
+<?php
+
+if(isset($_POST['submit_pad'])){
+    require_once('../php/Conectar.php');
+    $nombre = mysqli_real_escape_string($con, $_POST['NomPd']);
+    $sinto = mysqli_real_escape_string($con, $_POST['SintPd']);
+    $ejerc = mysqli_real_escape_string($con, $_POST['EjerPd']);
+    $otros = mysqli_real_escape_string($con, $_POST['OtroPd']);
+
+    if(!empty($nombre) || !empty($sinto) || !empty($ejerc) || !empty($otros)){
+
+        $sqlnom = "INSERT INTO Padecimientos (Nombre) VALUES('$nombre');";
+        $excute = mysqli_query($con,$sqlnom);
+        $sqls = "INSERT INTO PadCar (Categorias) VALUES('$sint');";
+        $sqle = "INSERT INTO PadCar (Categorias) VALUES('$ejerc');";
+        $sqlo = "INSERT INTO PadCar (Categorias) VALUES('$otros');";
+        $excuteS = mysqli_query($con,$sqls);
+        $excuteE = mysqli_query($con,$sqle);
+        $excuteO = mysqli_query($con,$sqlo);
+
+        if(!$excute || !$excuteS || !$excuteE || !$excuteO){
+            echo "Failed to submit the data";
+            exit();
+            //tengo que cambiar el url.
+        }else{
+            header('refresh:5; url=dashboard.php');
+            echo "Article Published succesfully";
+            exit();
+        }
+
+
+    }else{
+        header('Location: dashboard.php?emptyField');
+        exit();
+    }
+
+}else{
+    header('Location: dashboard.php?invalidRequest');
+    exit();
+}
+
+
+
+?>
