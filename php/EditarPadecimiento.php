ad421aab049aca457b5ec279492f1ec47cf780f8
GUERSON EMILIA <emiliaguerson@gmail.com>
2022-10-20 14:28:26 -0300

    Imágenes terapéuticas

--- php/EditarPadecimiento.php
+++ php/EditarPadecimiento.php
@@ -1,19 +1,19 @@
 <?php
 
-if(isset($_POST['submit_pad'])){
+if(isset($_POST['buscnom'])){
     require_once('../php/Conectar.php');
-    $nombre = mysqli_real_escape_string($con, $_POST['NomPd']);
-    $sinto = mysqli_real_escape_string($con, $_POST['SintPd']);
-    $ejerc = mysqli_real_escape_string($con, $_POST['EjerPd']);
-    $otros = mysqli_real_escape_string($con, $_POST['OtroPd']);
+    $nombre = mysqli_real_escape_string($con, $_POST['nomedit']);
+    $sinto = mysqli_real_escape_string($con, $_POST['SintTxt']);
+    $ejerc = mysqli_real_escape_string($con, $_POST['EjerTxt']);
+    $otros = mysqli_real_escape_string($con, $_POST['OtroTxt']);
 //Tengo que cambiar todas las sentencias por UPDATE y ajustar el código a eso
     if(!empty($nombre) || !empty($sinto) || !empty($ejerc) || !empty($otros)){
 
-        $sqlnom = "INSERT INTO Padecimientos (Nombre) VALUES('$nombre');";
+        $sqlnom = "UPDATE Padecimientos SET Nombre = ('$nombre') WHERE id = '".$_REQUEST['buscnom']."';";
         $excute = mysqli_query($con,$sqlnom);
-        $sqls = "INSERT INTO PadCar (Categorias) VALUES('$sint');";
-        $sqle = "INSERT INTO PadCar (Categorias) VALUES('$ejerc');";
-        $sqlo = "INSERT INTO PadCar (Categorias) VALUES('$otros');";
+        $sqls = "UPDATE PadCar SET Texto = ('$sint') WHERE id = '".$_REQUEST['buscnom']."' AND Categoria = 'Síntomas';";
+        $sqle = "UPDATE PadCar SET Texto = ('$ejerc') WHERE id = '".$_REQUEST['buscnom']."' AND Categoria = 'Ejercicios';";
+        $sqlo =  "UPDATE PadCar SET Texto = ('$otros') WHERE id = '".$_REQUEST['buscnom']."' AND Categoria = 'Ejercicios';";
         $excuteS = mysqli_query($con,$sqls);
         $excuteE = mysqli_query($con,$sqle);
         $excuteO = mysqli_query($con,$sqlo);
