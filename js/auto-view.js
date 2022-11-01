af31280df4a482128bd34518f3dfdf5a69be8238
GUERSON EMILIA <emiliaguerson@gmail.com>
2022-11-01 11:55:24 -0300

    ViewContent 2.0

---
+++ js/auto-view.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const autoInput = document.querySelectorAll('.autocomplete');
+
+    $.ajax("../autocomplete.php")
+        .done(function (data) {
+            M.Autocomplete.init(autoInput, {
+                data: JSON.parse(data)
+            })
+        })
+        .fail(function () {
+            alert("error");
+        })
+
+})
+
+$("#submitBtn").click(function(){
+alert ('ViewContent.php?que='+$("#buscnom").val()+'');
+    location.href ='../php/ViewContent.php?que='+$("#buscnom").val()+'';
+})
